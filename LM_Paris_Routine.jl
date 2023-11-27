11ee7e1173a1ea474fa3cf76753a9ac7efa4b6c7
everval <eduardo.vera@gmail.com>
2023-11-27 23:33:58 +0100

    Magic!

--- LM_Paris_Routine.jl
+++ LM_Paris_Routine.jl
@@ -8,28 +8,11 @@ fechas = [Date(2013, 1, 1) Date(2016, 11, 9) Date(2020, 1, 29) Date(2023, 2, 28)
 ## Loading data
 nm = length(markets)
 
-bds = 0.30:0.005:0.85
+bds = 0.35:0.005:0.85
 
 nbd = length(bds)
 
-
-### GPH estimation
-begin
-    tablon_pre_gph_rt = DataFrame()
-    tablon_pre_gph_lt = DataFrame()
-    tablon_pre_lw_rt = DataFrame()
-    tablon_pre_lw_lt = DataFrame()
-
-    tablon_post_gph_rt = DataFrame()
-    tablon_post_gph_lt = DataFrame()
-    tablon_post_lw_rt = DataFrame()
-    tablon_post_lw_lt = DataFrame()
-
-    tablon_covid_gph_rt = DataFrame()
-    tablon_covid_gph_lt = DataFrame()
-    tablon_covid_lw_rt = DataFrame()
-    tablon_covid_lw_lt = DataFrame()
-end
+tablita = DataFrame("Symbol" => [], "GPH" => [], "LW" => [], "Period" => [], "Bandwidth" => [], "ReturnType" => [])
 
 for ii = 1:nm
     println("Market: ", markets[ii])
@@ -55,10 +38,9 @@ for ii = 1:nm
         lw_lt[jj] = exact_whittle_est(values(prepa_lt); m=bds[jj])
     end
 
-    tablon_pre_gph_rt[!, markets[ii]] = gphs_rt[:]
-    tablon_pre_gph_lt[!, markets[ii]] = gphs_lt[:]
-    tablon_pre_lw_rt[!, markets[ii]] = lw_rt[:]
-    tablon_pre_lw_lt[!, markets[ii]] = lw_lt[:]
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "LW" => lw_rt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "Returns"))
+
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "LW" => lw_lt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns"))
 
     ### PostPA
 
@@ -76,11 +58,9 @@ for ii = 1:nm
         lw_lt[jj] = exact_whittle_est(values(postpa_lt); m=bds[jj])
     end
 
-    tablon_post_gph_rt[!, markets[ii]] = gphs_rt[:]
-    tablon_post_gph_lt[!, markets[ii]] = gphs_lt[:]
-    tablon_post_lw_rt[!, markets[ii]] = lw_rt[:]
-    tablon_post_lw_lt[!, markets[ii]] = lw_lt[:]
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "LW" => lw_rt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "Returns"))
 
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "LW" => lw_lt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns"))
     ### Covid
 
     covid_rt = to(from(rt, Date(fechas[3])), fechas[4])
@@ -97,30 +77,34 @@ for ii = 1:nm
         lw_lt[jj] = exact_whittle_est(values(covid_lt); m=bds[jj])
     end
 
-    tablon_covid_gph_rt[!, markets[ii]] = gphs_rt[:]
-    tablon_covid_gph_lt[!, markets[ii]] = gphs_lt[:]
-    tablon_covid_lw_rt[!, markets[ii]] = lw_rt[:]
-    tablon_covid_lw_lt[!, markets[ii]] = lw_lt[:]
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "LW" => lw_rt[:], "Period" => "Covid", "Bandwidth" => bds[:], "ReturnType" => "Returns"))
+
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "LW" => lw_lt[:], "Period" => "Covid", "Bandwidth" => bds[:], "ReturnType" => "LogReturns"))
 
 end
 
+CSV.write("LM_Paris_Routine.csv", tablita)
 
-tablon_pre_gph_rt[!, markets[ii]] = gphs_rt[:]
+begin
+tablita_rt_pre = choose_options(tablita, "PrePA", "Returns")
+tablita_rt_post = choose_options(tablita, "PostPA", "Returns")
+tablita_rt_covid = choose_options(tablita, "Covid", "Returns")
 
-tablita = DataFrame("Symbol" => [], "GPH" => [], "LW" => [], "Period" => [], "Bandwidth" => [], "ReturnType" => [])
+tablita_lt_pre = choose_options(tablita, "PrePA", "LogReturns")
+tablita_lt_post = choose_options(tablita, "PostPA", "LogReturns")
+tablita_lt_covid = choose_options(tablita, "Covid", "LogReturns")
 
-
-append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "LW" => lw_rt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "Returns"))
-
-append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "LW" => lw_lt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns"))
-
-@df tablon_pre_gph_rt violin(string.(":",markets[1]))
-
-
-violin
+end
 
 theme(:ggplot2)
-violin(tablon_covid_gph_lt[!,markets[1]])
+@df tablita_rt_pre violin(string.(":",markets), :LW, side=:left, label = "PrePA")
+@df tablita_rt_post violin!(string.(":",markets), :LW, side=:right, label = "PostPA")
+plot!(ylims = (-0.2, 0.8))
+#@df tablita_rt_pre dotplot!(string.(":",markets), :GPH, side=:left, label = "", marker=(:black,stroke(0)), mode=:uniform)
+#@df tablita_rt_post dotplot!(string.(":",markets), :GPH, side=:right, label = "", marker=(:gray,stroke(0)), mode=:uniform)
 
 
 
+function choose_options(tablita, period, return_type)
+    return tablita[(tablita.Period .== period) .& (tablita.ReturnType .== return_type), :]
+end
