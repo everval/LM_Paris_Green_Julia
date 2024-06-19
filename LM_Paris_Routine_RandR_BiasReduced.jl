ba6f6e1e565d9b25a72b91ae8a326f109c3b9521
everval <eduardo.vera@gmail.com>
2024-06-19 14:33:05 +0200

    New stocks

--- LM_Paris_Routine_RandR_BiasReduced.jl
+++ LM_Paris_Routine_RandR_BiasReduced.jl
@@ -6,8 +6,9 @@ Pkg.activate(pwd())
 using LongMemory, MarketData, StatsPlots, DataFrames, TimeSeries, CSV
 include("LM_Paris_Functions.jl")
 
-generalmarket = ["AAPL", "MSFT", "GOOG"]#, "AMZN", "BAC", "JPM", "NVO", "PFE", "NVDA", "INTC", "WBD", "NFLX", "DIS", "META", "PRU"]
-energy = ["CVX", "XOM", "BP"]#, "SHEL", "COP", "TTE", "TSLA", "F", "PLUG", "FSLR", "SPWR", "BEP", "VWS.CO", "EDPFY", "ORRON.ST", "GE", "CSIQ", "DNN", "CWEN", "GPRE", "SEDG", "SU", "ENPH", "NEE", "IFX.DE"]
+generalmarket = ["AAPL", "MSFT", "GOOG", "AMZN", "BAC", "JPM", "NVO", "PFE", "NVDA", "INTC", "WBD", "NFLX", "DIS", "META", "PRU"]
+#energy = ["ENPH", "NEE", "IFX.DE"]
+energy = ["CVX", "XOM", "BP", "SHEL", "COP", "TTE", "TSLA", "F", "PLUG", "FSLR", "SPWR", "BEP", "VWS.CO", "EDPFY", "ORRON.ST", "GE", "CSIQ", "DNN", "CWEN", "GPRE", "SEDG", "SU", "ENPH", "NEE", "IFX.DE"]
 
 markets = [generalmarket; energy]
 
@@ -20,7 +21,7 @@ bds = 0.3:0.005:0.85
 
 nbd = length(bds)
 
-tablita = DataFrame("Symbol" => [], "GPH" => [], "GPH_BR_1" => [], "LW" => [], "Period" => [], "Bandwidth" => [], "ReturnType" => [], "Market" => [])
+tablita = DataFrame("Symbol" => [], "GPH" => [], "GPH_BR_1" => [], "GPH_BR_2" => [], "LW" => [], "ELW" => [], "Period" => [], "Bandwidth" => [], "ReturnType" => [], "Market" => [])
 
 for ii = 1:nm
     println("Market: ", markets[ii])
@@ -38,138 +39,104 @@ for ii = 1:nm
     lt = log_returns(actual) .^ 2
 
     ### PrePA
-    prepa_rt = to(from(rt, Date(fechas[1])), fechas[2])
-    prepa_lt = to(from(lt, Date(fechas[1])), fechas[2])
+    prepa_rt = collect(skipmissing(values(to(from(rt, Date(fechas[1])), fechas[2]))))
+    prepa_lt = collect(skipmissing(values(to(from(lt, Date(fechas[1])), fechas[2]))))
 
     gphs_rt = zeros(nbd, 1)
     gphs_lt = zeros(nbd, 1)
     gph_br1_rt = zeros(nbd, 1)
     gph_br1_lt = zeros(nbd, 1)
+    gph_br2_rt = zeros(nbd, 1)
+    gph_br2_lt = zeros(nbd, 1)
     lw_rt = zeros(nbd, 1)
     lw_lt = zeros(nbd, 1)
+    elw_rt = zeros(nbd, 1)
+    elw_lt = zeros(nbd, 1)
     for jj = 1:nbd
         gphs_rt[jj] = gph_est(values(prepa_rt); m=bds[jj])
         gphs_lt[jj] = gph_est(values(prepa_lt); m=bds[jj])
         gph_br1_rt[jj] = gph_est(values(prepa_rt); m=bds[jj], br=1)
         gph_br1_lt[jj] = gph_est(values(prepa_lt); m=bds[jj], br=1)
-        lw_rt[jj] = exact_whittle_est(values(prepa_rt); m=bds[jj])
-        lw_lt[jj] = exact_whittle_est(values(prepa_lt); m=bds[jj])
+        gph_br2_rt[jj] = gph_est(values(prepa_rt); m=bds[jj], br=2)
+        gph_br2_lt[jj] = gph_est(values(prepa_lt); m=bds[jj], br=2)
+        lw_rt[jj] = whittle_est(values(prepa_rt); m=bds[jj])
+        lw_lt[jj] = whittle_est(values(prepa_lt); m=bds[jj])
+        elw_rt[jj] = exact_whittle_est(values(prepa_rt); m=bds[jj])
+        elw_lt[jj] = exact_whittle_est(values(prepa_lt); m=bds[jj])
     end
 
-    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "GPH_BR_1" => gph_br1_rt[:], "LW" => lw_rt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "Returns", "Market" => ind_market))
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "GPH_BR_1" => gph_br1_rt[:], "GPH_BR_2" => gph_br2_rt[:], "LW" => lw_rt[:], "ELW" => elw_rt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "Returns", "Market" => ind_market))
 
-    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "GPH_BR_1" => gph_br1_lt[:], "LW" => lw_lt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns", "Market" => ind_market))
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "GPH_BR_1" => gph_br1_lt[:], "GPH_BR_2" => gph_br2_lt[:], "LW" => lw_lt[:], "ELW" => elw_lt[:], "Period" => "PrePA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns", "Market" => ind_market))
 
     ### PostPA
 
-    postpa_rt = to(from(rt, Date(fechas[2])), fechas[3])
-    postpa_lt = to(from(lt, Date(fechas[2])), fechas[3])
+    postpa_rt = collect(skipmissing(values(to(from(rt, Date(fechas[2])), fechas[3]))))
+    postpa_lt = collect(skipmissing(values(to(from(lt, Date(fechas[2])), fechas[3]))))
 
     gphs_rt = zeros(nbd, 1)
     gphs_lt = zeros(nbd, 1)
     gph_br1_rt = zeros(nbd, 1)
     gph_br1_lt = zeros(nbd, 1)
+    gph_br2_rt = zeros(nbd, 1)
+    gph_br2_lt = zeros(nbd, 1)
     lw_rt = zeros(nbd, 1)
     lw_lt = zeros(nbd, 1)
+    elw_rt = zeros(nbd, 1)
+    elw_lt = zeros(nbd, 1)
     for jj = 1:nbd
         gphs_rt[jj] = gph_est(values(postpa_rt); m=bds[jj])
         gphs_lt[jj] = gph_est(values(postpa_lt); m=bds[jj])
         gph_br1_rt[jj] = gph_est(values(postpa_rt); m=bds[jj], br=1)
         gph_br1_lt[jj] = gph_est(values(postpa_lt); m=bds[jj], br=1)
-        lw_rt[jj] = exact_whittle_est(values(postpa_rt); m=bds[jj])
-        lw_lt[jj] = exact_whittle_est(values(postpa_lt); m=bds[jj])
+        gph_br2_rt[jj] = gph_est(values(postpa_rt); m=bds[jj], br=2)
+        gph_br2_lt[jj] = gph_est(values(postpa_lt); m=bds[jj], br=2)
+        lw_rt[jj] = whittle_est(values(postpa_rt); m=bds[jj])
+        lw_lt[jj] = whittle_est(values(postpa_lt); m=bds[jj])
+        elw_rt[jj] = exact_whittle_est(values(postpa_rt); m=bds[jj])
+        elw_lt[jj] = exact_whittle_est(values(postpa_lt); m=bds[jj])
     end
 
-    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "GPH_BR_1" => gph_br1_rt[:], "LW" => lw_rt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "Returns", "Market" => ind_market))
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "GPH_BR_1" => gph_br1_rt[:], "GPH_BR_2" => gph_br2_rt[:], "LW" => lw_rt[:], "ELW" => elw_rt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "Returns", "Market" => ind_market))
 
-    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "GPH_BR_1" => gph_br1_lt[:], "LW" => lw_lt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns", "Market" => ind_market))
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "GPH_BR_1" => gph_br1_lt[:], "GPH_BR_2" => gph_br2_lt[:], "LW" => lw_lt[:], "ELW" => elw_lt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns", "Market" => ind_market))
     ### Covid
 
-    covid_rt = to(from(rt, Date(fechas[3])), fechas[4])
-    covid_lt = to(from(lt, Date(fechas[3])), fechas[4])
+    covid_rt = collect(skipmissing(values(to(from(rt, Date(fechas[3])), fechas[4]))))
+    covid_lt = collect(skipmissing(values(to(from(lt, Date(fechas[3])), fechas[4]))))
 
     gphs_rt = zeros(nbd, 1)
     gphs_lt = zeros(nbd, 1)
     gph_br1_rt = zeros(nbd, 1)
     gph_br1_lt = zeros(nbd, 1)
+    gph_br2_rt = zeros(nbd, 1)
+    gph_br2_lt = zeros(nbd, 1)
     lw_rt = zeros(nbd, 1)
     lw_lt = zeros(nbd, 1)
+    elw_rt = zeros(nbd, 1)
+    elw_lt = zeros(nbd, 1)
     for jj = 1:nbd
         gphs_rt[jj] = gph_est(values(covid_rt); m=bds[jj])
         gphs_lt[jj] = gph_est(values(covid_lt); m=bds[jj])
         gph_br1_rt[jj] = gph_est(values(covid_rt); m=bds[jj], br=1)
         gph_br1_lt[jj] = gph_est(values(covid_lt); m=bds[jj], br=1)
-        lw_rt[jj] = exact_whittle_est(values(covid_rt); m=bds[jj])
-        lw_lt[jj] = exact_whittle_est(values(covid_lt); m=bds[jj])
+        gph_br2_rt[jj] = gph_est(values(covid_rt); m=bds[jj], br=2)
+        gph_br2_lt[jj] = gph_est(values(covid_lt); m=bds[jj], br=2)
+        lw_rt[jj] = whittle_est(values(covid_rt); m=bds[jj])
+        lw_lt[jj] = whittle_est(values(covid_lt); m=bds[jj])
+        elw_rt[jj] = exact_whittle_est(values(covid_rt); m=bds[jj])
+        elw_lt[jj] = exact_whittle_est(values(covid_lt); m=bds[jj])
     end
 
-    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "GPH_BR_1" => gph_br1_rt[:], "LW" => lw_rt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "Returns", "Market" => ind_market))
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_rt[:], "GPH_BR_1" => gph_br1_rt[:], "GPH_BR_2" => gph_br2_rt[:], "LW" => lw_rt[:], "ELW" => elw_rt[:], "Period" => "Covid", "Bandwidth" => bds[:], "ReturnType" => "Returns", "Market" => ind_market))
 
-    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "GPH_BR_1" => gph_br1_lt[:], "LW" => lw_lt[:], "Period" => "PostPA", "Bandwidth" => bds[:], "ReturnType" => "LogReturns", "Market" => ind_market))
+    append!(tablita, DataFrame("Symbol" => markets[ii], "GPH" => gphs_lt[:], "GPH_BR_1" => gph_br1_lt[:], "GPH_BR_2" => gph_br2_lt[:], "LW" => lw_lt[:], "ELW" => elw_lt[:], "Period" => "Covid", "Bandwidth" => bds[:], "ReturnType" => "LogReturns", "Market" => ind_market))
 
 end
 
 CSV.write("Results/LM_Paris_Routine_RandR.csv", tablita)
 
+#=
 
+## More stocks
 
-
-begin
-    tablita_rt_pre = choose_options(tablita, "PrePA", "Returns")
-    tablita_rt_post = choose_options(tablita, "PostPA", "Returns")
-    tablita_rt_covid = choose_options(tablita, "Covid", "Returns")
-
-    tablita_lt_pre = choose_options(tablita, "PrePA", "LogReturns")
-    tablita_lt_post = choose_options(tablita, "PostPA", "LogReturns")
-    tablita_lt_covid = choose_options(tablita, "Covid", "LogReturns")
-
-end
-
-te = choose_options_market_bdws(tablita, "PrePA", "Returns", "Energy", [0.45, 0.46])
-
-begin
-theme(:ggplot2)
-p0 = @df tablita_lt_pre violin(markets, :GPH, side=:left, label="PrePA")
-@df tablita_lt_post violin!(markets, :GPH, side=:right, label="PostPA")
-plot!(ylims=(-0.2, 0.7), size=(1200, 500), legend=:topleft, title="Log-Returns, Exact Whittle Estimator, General Stocks")
-display(p0)
-#@df tablita_rt_pre dotplot!(string.(":",markets), :GPH, side=:left, label = "", marker=(:black,stroke(0)), mode=:uniform)
-#@df tablita_rt_post dotplot!(string.(":",markets), :GPH, side=:right, label = "", marker=(:gray,stroke(0)), mode=:uniform)
-end
-
-begin
-    tablita_rt_pre_general = choose_options_market(tablita, "PrePA", "Returns", "General")
-    tablita_rt_post_general = choose_options_market(tablita, "PostPA", "Returns", "General")
-    tablita_rt_covid_general = choose_options_market(tablita, "Covid", "Returns", "General")
-
-    tablita_lt_pre_general = choose_options_market(tablita, "PrePA", "LogReturns", "General")
-    tablita_lt_post_general = choose_options_market(tablita, "PostPA", "LogReturns", "General")
-    tablita_lt_covid_general = choose_options_market(tablita, "Covid", "LogReturns", "General")
-
-    tablita_rt_pre_energy = choose_options_market(tablita, "PrePA", "Returns", "Energy")
-    tablita_rt_post_energy = choose_options_market(tablita, "PostPA", "Returns", "Energy")
-    tablita_rt_covid_energy = choose_options_market(tablita, "Covid", "Returns", "Energy")
-
-    tablita_lt_pre_energy = choose_options_market(tablita, "PrePA", "LogReturns", "Energy")
-    tablita_lt_post_energy = choose_options_market(tablita, "PostPA", "LogReturns", "Energy")
-    tablita_lt_covid_energy = choose_options_market(tablita, "Covid", "LogReturns", "Energy")
-end
-
-begin
-    theme(:ggplot2)
-    p1 = @df tablita_lt_pre_general violin(markets, :LW, side=:left, label="PrePA")
-    @df tablita_lt_post_general violin!(markets, :LW, side=:right, label="PostPA")
-    plot!(ylims=(-0.15, 0.5), size=(1200, 500), legend=:topleft, title="Log-Returns, Exact Whittle Estimator, General Stocks")
-    display(p1)
-end
-
-
-begin
-    theme(:ggplot2)
-    p2 = @df tablita_lt_pre_energy violin(markets, :LW, side=:left, label="PrePA")
-    @df tablita_lt_post_energy violin!(markets, :LW, side=:right, label="PostPA")
-    plot!(ylims=(-0.15, 0.6), size=(1200, 500), legend=:topleft, title="Log-Returns, Exact Whittle Estimator, Energy Stocks")
-    display(p2)
-end
-
-=#
